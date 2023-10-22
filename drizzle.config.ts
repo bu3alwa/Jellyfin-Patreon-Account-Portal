ec1195f202054c30fa56677f5dbe59ad38d80340
Ali Ali <ali.bu3alwa@gmail.com>
2023-10-22 00:11:10 -0400

    chore: drizzle config should use process.env only

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "drizzle-kit";
-import { env } from "~/env.mjs";
 
 export default {
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
   driver: "better-sqlite",
   dbCredentials: {
-    url: env.DOCKER === "true" ? "/config/sqlite.db" : "./sqlite.db",
+    // This is sometimes is used in drizzle scripts where the env variables
+    // are not available. So don't use the env validation here and instead
+    // just use process.env
+    url: process.env.DOCKER === "true" ? "/config/sqlite.db" : "./sqlite.db",
   },
 } satisfies Config;
--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,8 +1,9 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
+import drizzleConfig from "drizzle.config";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database(drizzleConfig.dbCredentials.url);
 export const db = drizzle(sqlite, {
   schema,
 });
